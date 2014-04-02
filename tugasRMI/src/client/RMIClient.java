7510506198a81112c263df3a2234336b73abe924
Anggarda Diajeng Prameswari <ajprameswari@gmail.com>
2014-04-02 21:26:56 +0700

    fixing

--- tugasRMI/src/client/RMIClient.java
+++ tugasRMI/src/client/RMIClient.java
@@ -17,9 +17,10 @@ public class RMIClient {
     public static void printCuacaDunia(Document xml) {
         xml.getDocumentElement().normalize();
         NodeList nList = xml.getElementsByTagName("Row"), nList2 = xml.getElementsByTagName("Tanggal");
-        Element tanggal = (Element) xml.getElementsByTagName("Tanggal");
-        System.out.println("***Perkiraan Cuaca Dunia***");
-        System.out.println("Tanggal\t\t: " + tanggal.getTextContent());
+        Element tanggal = (Element) nList2.item(0);
+        System.out.println("\n***Perkiraan Cuaca Dunia***");
+        System.out.println("Tanggal: " + tanggal.getTextContent());
+        System.out.println("");
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -36,9 +37,10 @@ public class RMIClient {
         xml.getDocumentElement().normalize();
         NodeList nList = xml.getElementsByTagName("Row"), nList2 = xml.getElementsByTagName("Tanggal");
         Element tanggal = (Element) nList2.item(0);
-        System.out.println("***Perkiraan Cuaca Indonesia***");
+        System.out.println("\n***Perkiraan Cuaca Indonesia***");
         System.out.println("Tanggal Mulai\t: " + tanggal.getElementsByTagName("Mulai").item(0).getTextContent());
         System.out.println("Tanggal Sampai\t: " + tanggal.getElementsByTagName("Sampai").item(0).getTextContent());
+        System.out.println("");
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -49,6 +51,77 @@ public class RMIClient {
                     System.out.println("Suhu Max" + "\t: " + eElement.getElementsByTagName("SuhuMax").item(0).getTextContent());
                     System.out.println("Kelembapan Min" + "\t: " + eElement.getElementsByTagName("KelembapanMin").item(0).getTextContent());
                     System.out.println("Kelembapan Max" + "\t: " + eElement.getElementsByTagName("KelembapanMax").item(0).getTextContent() + "\n");
+                }
+            }
+    }
+    
+    public static void printCuacaJabodetabek(Document xml) {
+        xml.getDocumentElement().normalize();
+        NodeList nList = xml.getElementsByTagName("Row"), nList2 =  xml.getElementsByTagName("Tanggal");
+        Element tanggal = (Element) nList2.item(0);
+        System.out.println("\n***Perkiraan Cuaca Jabodetabek***");
+        System.out.println("Tanggal: " + tanggal.getTextContent());
+        System.out.println("");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Daerah" + "\t: " + eElement.getElementsByTagName("Daerah").item(0).getTextContent());
+                    System.out.println("Pagi" + "\t: " + eElement.getElementsByTagName("Pagi").item(0).getTextContent());
+                    System.out.println("Siang" + "\t: " + eElement.getElementsByTagName("Siang").item(0).getTextContent());
+                    System.out.println("Malam" + "\t: " + eElement.getElementsByTagName("Malam").item(0).getTextContent() + "\n");
+                }
+            }
+    }
+    
+    public static void printCuacaHarian(Document xml) {
+        xml.getDocumentElement().normalize();
+        NodeList nList = xml.getElementsByTagName("Row"), nList2 = xml.getElementsByTagName("Date");
+        Element tanggal = (Element) nList2.item(0);
+        System.out.println("\n***Perkiraan Cuaca Harian***");
+        System.out.println("Update\t\t\t: " + tanggal.getElementsByTagName("Update").item(0).getTextContent());
+        System.out.println("Jam Update\t\t: " + tanggal.getElementsByTagName("JamUpdate").item(0).getTextContent());
+        System.out.println("");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Valid" + "\t\t\t: " + eElement.getElementsByTagName("Valid").item(0).getTextContent());
+                    System.out.println("Weather" + "\t\t\t: " + eElement.getElementsByTagName("Weather").item(0).getTextContent());
+                    System.out.println("Wind Direction" + "\t\t: " + eElement.getElementsByTagName("WindDirection").item(0).getTextContent());
+                    System.out.println("Wind Speed" + "\t\t: " + eElement.getElementsByTagName("WindSpeed").item(0).getTextContent());
+                    System.out.println("Temperature" + "\t\t: " + eElement.getElementsByTagName("Temperature").item(0).getTextContent());
+                    System.out.println("Humidity" + "\t\t: " + eElement.getElementsByTagName("Humidity").item(0).getTextContent());
+                    System.out.println("Sun Rise" + "\t\t: " + eElement.getElementsByTagName("Sunrise").item(0).getTextContent());
+                    System.out.println("Sun Set" + "\t\t\t: " + eElement.getElementsByTagName("Sunset").item(0).getTextContent());
+                    System.out.println("Moon Rise" + "\t\t: " + eElement.getElementsByTagName("Moonrise").item(0).getTextContent());
+                    System.out.println("Moon Set" + "\t\t: " + eElement.getElementsByTagName("Moonset").item(0).getTextContent());
+                    System.out.println("Wave Height North Coast" + "\t: " + eElement.getElementsByTagName("WaveHeightNortCoast").item(0).getTextContent());
+                    System.out.println("Wave Height South Coast" + "\t: " + eElement.getElementsByTagName("WaveHeightSouthCoast").item(0).getTextContent() + "\n");
+                  
+                }
+            }
+    }
+    
+    public static void printCuacaWisata(Document xml) {
+        xml.getDocumentElement().normalize();
+        NodeList nList = xml.getElementsByTagName("Row"), nList2 = xml.getElementsByTagName("Date");
+        Element tanggal = (Element) nList2.item(0);
+        System.out.println("\n***Perkiraan Cuaca Wisata di Bali***");
+        System.out.println("Valid Start\t: " + tanggal.getElementsByTagName("ValidStart").item(0).getTextContent());
+        System.out.println("Valid Time Start: " + tanggal.getElementsByTagName("ValidTimeStart").item(0).getTextContent());
+        System.out.println("Valid End\t: " + tanggal.getElementsByTagName("ValidEnd").item(0).getTextContent());
+        System.out.println("Valid Time End\t: " + tanggal.getElementsByTagName("ValidTimeEnd").item(0).getTextContent());
+        System.out.println("");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Area" + "\t\t: " + eElement.getElementsByTagName("Area").item(0).getTextContent());
+                    System.out.println("Weather" + "\t\t: " + eElement.getElementsByTagName("Weather").item(0).getTextContent());
+                    System.out.println("Temperature" + "\t: " + eElement.getElementsByTagName("Temperature").item(0).getTextContent());
+                    System.out.println("Humidity" + "\t: " + eElement.getElementsByTagName("Humidity").item(0).getTextContent());
+                    System.out.println("Wave Height" + "\t: " + eElement.getElementsByTagName("WaveHeight").item(0).getTextContent() + "\n");
                 }
             }
     }
@@ -73,7 +146,7 @@ public class RMIClient {
     
             switch(pil){
                 case 1: 
-                        url="http://data.bmkg.go.id/cuaca_dunia_1.xml";
+                        url="http://data.bmkg.go.id/cuaca_dunia_2.xml";
                         xmldata = cInterface.getCuaca(url);
                         printCuacaDunia(xmldata);
                         break;
@@ -85,17 +158,17 @@ public class RMIClient {
                 case 3: 
                         url="http://data.bmkg.go.id/cuaca_jabodetabek_1.xml";
                         xmldata = cInterface.getCuaca(url);
-                        System.out.println(xmldata);
+                        printCuacaJabodetabek(xmldata);
                         break;
                 case 4: 
                         url="http://data.bmkg.go.id/cuaca_harian_id.xml";
                         xmldata = cInterface.getCuaca(url);
-                        System.out.println(xmldata);
+                        printCuacaHarian(xmldata);
                         break;
                 case 5: 
                         url="http://data.bmkg.go.id/cuaca_wisata.xml";
                         xmldata = cInterface.getCuaca(url);
-                        System.out.println(xmldata);
+                        printCuacaWisata(xmldata);
                         break;
                 case 6: break;
                 default: System.out.println("Harap pilih dengan benar: "); break;
